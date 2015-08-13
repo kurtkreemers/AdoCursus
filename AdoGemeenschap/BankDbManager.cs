d1b6276efaaba9bd931f2eacdee16d14dd99cedc
Kurt Kreemers <kkreeme1@gmail.com>
2015-08-13 08:54:54 +0200

    12082015

---
+++ AdoGemeenschap/BankDbManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoGemeenschap
+{
+    public class BankDbManager
+    {
+        private static ConnectionStringSettings conBankSetting = ConfigurationManager.ConnectionStrings["Bank"];
+        private static DbProviderFactory factory = DbProviderFactories.GetFactory(conBankSetting.ProviderName);
+
+        public DbConnection GetConnection()
+        {
+            var conBank = factory.CreateConnection();
+            conBank.ConnectionString = conBankSetting.ConnectionString;
+            return conBank;
+        }
+    }
+}
---
+++ AdoGemeenschap/RekeningenManager.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Common;
+using System.Transactions;
+
+namespace AdoGemeenschap
+{
+    public class RekeningenManager
+    {
+        public Int32 SaldoBonus()
+        {
+            var dbManager = new BankDbManager();
+            using (var conBank = dbManager.GetConnection())
+            {
+                using (var comBonus = conBank.CreateCommand())
+                {
+                    comBonus.CommandType = CommandType.Text;
+                    comBonus.CommandText = "update Rekeningen set Saldo=Saldo*1.1";
+                    conBank.Open();
+                    return comBonus.ExecuteNonQuery();
+                }
+            }
+        }
+        public Boolean Storten(decimal teStorten, string rekeningNr)
+        {
+            var dbManager = new BankDbManager();
+            using (var conBank = dbManager.GetConnection())
+            {
+                using (var comStorten = conBank.CreateCommand())
+                {
+                    comStorten.CommandType = CommandType.StoredProcedure;
+                    comStorten.CommandText = "Storten";
+
+                    DbParameter parTeStorten = comStorten.CreateParameter();
+                    parTeStorten.ParameterName = "@teStorten";
+                    parTeStorten.Value = teStorten;
+                    parTeStorten.DbType = DbType.Currency;
+                    comStorten.Parameters.Add(parTeStorten);
+
+                    DbParameter parRekeningNr = comStorten.CreateParameter();
+                    parRekeningNr.ParameterName = "@RekeningNr";
+                    parRekeningNr.Value = rekeningNr;
+                    comStorten.Parameters.Add(parRekeningNr);
+
+                    conBank.Open();
+                    return comStorten.ExecuteNonQuery() != 0;
+                }
+            }
+        }
+        public void Overschrijven(decimal bedrag, string vanRekening, string naarRekening)
+        {
+            var dbManager = new BankDbManager();
+            var dbManager2 = new Bank2DbManager();
+
+            var opties = new TransactionOptions();
+            opties.IsolationLevel = System.Transactions.IsolationLevel.ReadCommitted;
+
+            using (var traOverschrijven = new TransactionScope(TransactionScopeOption.Required,opties))
+            {
+                using (var conBank = dbManager.GetConnection())
+                {
+                    using (var comAftrekken = conBank.CreateCommand())
+                    {
+                        comAftrekken.CommandType = CommandType.Text;
+                        comAftrekken.CommandText = "update Rekeningen set Saldo=Saldo-@bedrag where RekeningNr=@reknr";
+
+                        var parBedrag = comAftrekken.CreateParameter();
+                        parBedrag.ParameterName = "@bedrag";
+                        parBedrag.Value = bedrag;
+                        comAftrekken.Parameters.Add(parBedrag);
+
+                        var parRekNr = comAftrekken.CreateParameter();
+                        parRekNr.ParameterName = "@reknr";
+                        parRekNr.Value = vanRekening;
+                        comAftrekken.Parameters.Add(parRekNr);
+
+                        conBank.Open();
+                        if (comAftrekken.ExecuteNonQuery() == 0)
+                        {
+                            throw new Exception("Van rekening bestaat niet");
+                        }
+                    }
+
+                }
+                using (var conBank = dbManager2.GetConnection())
+                {
+                    using (var comBijtellen = conBank.CreateCommand())
+                    {
+                        comBijtellen.CommandType = CommandType.Text;
+                        comBijtellen.CommandText = "update Rekeningen set Saldo=Saldo+@bedrag where RekeningNr=@reknr";
+
+                        var parBedrag = comBijtellen.CreateParameter();
+                        parBedrag.ParameterName = "@bedrag";
+                        parBedrag.Value = bedrag;
+                        comBijtellen.Parameters.Add(parBedrag);
+
+                        var parRekNr = comBijtellen.CreateParameter();
+                        parRekNr.ParameterName = "@reknr";
+                        parRekNr.Value = naarRekening;
+                        comBijtellen.Parameters.Add(parRekNr);
+
+                        conBank.Open();
+                        if (comBijtellen.ExecuteNonQuery() == 0)
+                        {
+                            throw new Exception("Naar rekening bestaat niet");
+                        }
+                        traOverschrijven.Complete();
+                    }
+                }
+            }
+            
+         }  
+            
+    }
+}
