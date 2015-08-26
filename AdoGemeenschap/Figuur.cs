35e8a1e14c17acf1756298eb7f1ec937ce5ebc27
Kurt Kreemers <kkreeme1@gmail.com>
2015-08-26 12:12:06 +0200

    26082015

---
+++ AdoGemeenschap/Figuur.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoGemeenschap
+{
+    public class Figuur
+    {
+        private Int32 IDValue;
+        private String naamValue;
+        private System.Object versieValue;
+        public bool Changed { get; set; }
+        public Int32 ID
+        {
+            get { return IDValue; }
+            set { IDValue = value; }
+        }
+        public String Naam
+        {
+            get { return naamValue; }
+            set { 
+                naamValue = value;
+                Changed = true;
+            }
+        }
+        public Object Versie
+        {
+            get { return versieValue; }
+            set { versieValue = value; }
+        }
+        public Figuur(Int32 id, String naam, Object versie)
+        {
+            this.IDValue = id;
+            this.Naam = naam;
+            this.Versie = versie;
+            this.Changed = false;
+        }
+        public Figuur() { }
+    }
+}
